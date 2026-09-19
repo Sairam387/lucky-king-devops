491d75d0b05071e59c7496555ebcd9365e754b8d
Sairam T <sairam@CSMADC.AD>
2026-09-19 15:25:45 +0530

    Add JUnit automated test

---
+++ src/test/java/com/sairam/game/LuckyKingBasicTest.java
@@ -0,0 +1,14 @@
+package com.sairam.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class LuckyKingBasicTest {
+
+    @Test
+    void additionTest() {
+        int result = 1000 + 500;
+
+        assertEquals(1500, result);
+    }
+}
